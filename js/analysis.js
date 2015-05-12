e0d0701a112b6c58d81b8ac8689288ce5a8a0229
Steve Leathers <sayhello@steveleathers.com>
2015-05-12 13:16:07 -0700

    joined data with filter method

--- js/analysis.js
+++ js/analysis.js
@@ -13,3 +13,47 @@ d3.csv("/data/bikethefts20072014.csv", function(data){
 	});
 	console.log(data[0]);
 });
+
+var articles = [{
+    "id": 1,
+    "name": "vacuum cleaner",
+    "weight": 9.9,
+    "price": 89.9,
+    "brand_id": 2
+}, {
+    "id": 2,
+    "name": "washing machine",
+    "weight": 540,
+    "price": 230,
+    "brand_id": 1
+}, {
+    "id": 3,
+    "name": "hair dryer",
+    "weight": 1.2,
+    "price": 24.99,
+    "brand_id": 2
+}, {
+    "id": 4,
+    "name": "super fast laptop",
+    "weight": 400,
+    "price": 899.9,
+    "brand_id": 3
+}];
+
+var brands = [{
+    "id": 1,
+    "name": "SuperKitchen"
+}, {
+    "id": 2,
+    "name": "HomeSweetHome"
+}];
+
+articles.forEach(function(article){
+	var result = brands.filter(function(brand){
+		return brand.id === article.brand_id;
+	});
+	delete article.brand_id;
+	article.brand = (result[0] !== undefined) ? result[0].name : null;
+});
+
+console.log(articles)
