970a26f73dc37f342a8e0b1bcb25a2315dacd0fc
Steve Leathers <sayhello@steveleathers.com>
2015-05-12 12:56:29 -0700

    read data

--- js/analysis.js
+++ js/analysis.js
@@ -1,3 +1,15 @@
 d3.csv("/data/bikethefts20072014.csv", function(data){
+	data.forEach(function(d){
+		d["Address Type"] = +d["Address Type"];
+		d["Case Year"] = +d["Case Year"];
+		d["Case Num"] = +d["Case Num"];
+		d.District = +d.District;
+		d.Status = +d.Status;
+		d.X = +d.X;
+		d.Y = +d.Y;
+		d.ZIP_Code = +d.ZIP_Code;
+		d.lat = +d.lat;
+		d.lng = +d.lng;
+	});
 	console.log(data[0]);
 });
